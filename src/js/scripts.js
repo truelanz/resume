96b27fe655b35d477a7a3f9ce9c50f4930433858
truelanz <devalanoliveira@gmail.com>
2023-05-09 00:30:16 -0300

    add some responsive, links and refactoring.

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,7 +2,9 @@
 import "../css/global.style.css";
 import "../css/changer-theme.css";
 import "../css/light-theme.css";
+import "../css/responsive.css";
 
+/* theme changer */
 const toggleButton = document.querySelector('#changer');
 const body = document.body;
 
@@ -10,3 +12,21 @@ toggleButton.addEventListener('click', () => {
   body.classList.toggle('dark');
   body.classList.toggle('light');
 });
+
+
+/* pop-up */
+var links = document.querySelectorAll('.alert-link');
+links.forEach(function (link) {
+  link.addEventListener('click', function (event) {
+    event.preventDefault();
+    Swal.fire({
+      title: 'Página em construção, por favor, acesse o github para mais informações.',
+      showClass: {
+        popup: 'animate__animated animate__fadeInDown'
+      },
+      hideClass: {
+        popup: 'animate__animated animate__fadeOutUp'
+      }
+    })
+  })
+})
